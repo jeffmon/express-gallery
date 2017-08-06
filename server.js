04e4605cdf04c234f91359b39c09b595126e0192
jeffmon <jeffmon@gmail.com>
2017-08-05 14:52:48 -1000

    first init

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const db = require("./models");
+const app = express();
+const bp = require("body-parser");
+app.use(bp.urlencoded());
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}
